d884b635786604d3b09325ed0856ae2ff56c6705
Sujit Kumar Muduli <smuduli@iitk.ac.in>
2020-07-01 11:45:24 +0530

    adding commets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,10 +10,16 @@ object Main {
     val c = Lit(2);
     val d = Lit(3);
 
-    val rel = Equal(Add(a, b), Lit(30))
-    println(rel.toString)
+    /*
+      if (10 + 20 != 30) {
+        x := 2
+      } else {
+        x := 3
+      }
+     */
 
     val varX = IntVar("x")
+    val rel = Equal(Add(a, b), Lit(30))
     val thenBody = AssignStmt(varX, c);
     val elseBody = AssignStmt(varX, d);
     val ifStmt = IfStmt(Not(rel), List(thenBody), List(elseBody))
@@ -21,10 +27,20 @@ object Main {
     Interpreter.executeStmt(ifStmt);
     println(varX.toString + " = " + Interpreter.valueOf(varX))
 
+
+
+    /*
+
+    while(x < 20 {
+      x := x + 1;
+    }
+     */
+
     val whileCond = Lesser(varX, b)
     val whileBody = AssignStmt(varX, Add(varX, Lit(1)))
     val whileStmt = WhileStmt(whileCond, List(whileBody))
 
+    Interpreter.executeStmt(AssignStmt(varX, Lit(0)))
     Interpreter.executeStmt(whileStmt)
     println(varX.toString + " = " + Interpreter.valueOf(varX))
 
